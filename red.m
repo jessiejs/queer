3c0c9e702f008a3e9f397c6ca5bd84f8b0c871a7
jessie <codelikecraze@gmail.com>
2023-06-10 17:42:26 -0700

    Red = 50

--- red.m
+++ red.m
@@ -1,18 +1,6 @@
-:- module servlet.
-
-:- interface.
-
-:- import_module io.
-
-:- type request.
-:- type response.
-
-:- implementation.
-
-:- import_module list.
-:- import_module string.
-
-handle_get(Req, Response, !IO) :-
-    set_content_type(Response, "text/html", !IO),
-    Msg = "Be gay, and do crimes! 🏴",
-    write_response(Response, Msg, !IO).
+aaaaaaaaaa
+aaaaaaaaaa
+aaaaaaaaaa
+aaaaaaaaaa
+aaaaaaaaaa
+aaaaaaaaaa
